e9bdd973bc3e5d8ab9fb8f516ed15f1b11868912
hari <harischetan717@gmail.com>
2023-09-01 08:17:19 +0530

    updated

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
       "https://aviad.vercel.app",
-      "http://localhost:3001",
+
       "https://appavi.vercel.app",
       "https://avir.vercel.app",
     ], // Change this to your frontend's URL
